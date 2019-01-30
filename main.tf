1efa1b6cfeade29251f1cff748edab6602aceec8
Breimers <b.a.reimers@gmail.com>
2019-01-30 00:10:07 -0800

    Added Hello World

--- main.tf
+++ main.tf
@@ -5,9 +5,25 @@ provider "aws" {
 resource "aws_instance" "ch2test" {
   ami	= "ami-40d4f025"
   instance_type ="t2.micro"
+  vpc_security_group_ids=["${aws_security_group.instance.id}"]
   tags {
     Name = "terraform-example"
   }
+  
+  user_data = <<-EOF
+	    #!/bin/bash
+	    echo "Hello, World!" > index.html
+	    nohup busybox httpd -f -p 8080 &
+	    EOF
 }
 
-
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress{
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+}
