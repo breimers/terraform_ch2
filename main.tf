fecdcc046a83ba0bec093077ee62b7735dac5309
Breimers <b.a.reimers@gmail.com>
2019-01-30 20:11:30 -0800

    Added Variable 'serverport'

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region= "us-east-2"
 }
 
+variable "serverport"{
+  description = "Sets the default port for contacting server"
+  default = 8080
+}
+
 resource "aws_instance" "ch2test" {
   ami	= "ami-40d4f025"
   instance_type ="t2.micro"
@@ -13,7 +18,7 @@ resource "aws_instance" "ch2test" {
   user_data = <<-EOF
 	    #!/bin/bash
 	    echo "Hello, World!" > index.html
-	    nohup busybox httpd -f -p 8080 &
+	    nohup busybox httpd -f -p "${var.serverport}" &
 	    EOF
 }
 
@@ -21,9 +26,32 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   
   ingress{
-    from_port = 8080
-    to_port   = 8080
+    from_port = "${var.serverport}"
+    to_port   = "${var.serverport}"
     protocol  = "tcp"
     cidr_blocks=["0.0.0.0/0"]
   }
 }
+#
+#resource "aws_route53_zone" "main"{
+#  name = "bradsbox.info"
+#}
+#resource "aws_route53_zone" "ch2"{
+#  name = "ch2.bradsbox.info"
+#  tags = {
+#    Environment = "ch2"
+#  }
+#}
+#resource "aws_route53_record" "ch2-ns"{
+#  zone_id = "${aws_route53_zone.main.zone_id}"
+#  name = "ch2.bradsbox.info"
+#  type = "NS"
+#  ttl = "300"
+#
+#  records = [
+#	"${aws_route53_zone.ch2.name_servers.0}",
+#        "${aws_route53_zone.ch2.name_servers.1}",
+#        "${aws_route53_zone.ch2.name_servers.2}",
+#        "${aws_route53_zone.ch2.name_servers.3}",
+#    ]
+#}
